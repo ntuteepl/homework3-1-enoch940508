dbbfb27820372813777a58c8e56bfeb0d1242170
enoch940508 <146065382+enoch940508@users.noreply.github.com>
2023-10-24 12:53:36 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int is_prime(int);
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	if(is_prime(n)){
+		printf("yes\n");
+	}else{
+		printf("no\n");
+	}
+
+  system("PAUSE");
+  return 0;
+}
+int is_prime(int num){
+	int i;
+	if(num==1){
+		return 0;
+	}
+	else
+    {
+       for(i=2;i<num;i++)
+       {
+			if(num%i==0)
+			{
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
